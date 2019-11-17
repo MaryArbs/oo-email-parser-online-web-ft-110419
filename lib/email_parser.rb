dc0981e70892197ec77a1cd6d09261956497185d
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-17 18:56:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,13 +3,13 @@ class EmailAddressParser
 
 attr_accessor :email_address
 
-def initialize(email_addresse)
+def initialize(email_address)
   @email_address=email_address
 end
 
 def parse
-email_address.collect do |address|
- address.split(',')
+email_address.split.collect do |address|
+ address.split(",")
 end
 .flatten.uniq
    end
